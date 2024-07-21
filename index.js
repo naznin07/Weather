875e524978a7155bdd9dcf976693548dc59844df
naznin07 <naffect1@gmail.com>
2024-07-21 11:29:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
-
+  cityElement.innerHTML = response.data.location.name;
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.current.temperature;
-  let descriptionElement = document.querySelector("#description");
+  let temperature = response.data.current.temp_c;
 
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.location.localtime);
-  console.log(response.data);
-  cityElement.innerHTML = response.data.request.query;
-
-  timeElement.innerHTML = formatDate(date);
-  temperatureElement.innerHTML = Math.round(temperature);
-  descriptionElement.innerHTML = response.data.current.weather_descriptions;
+  windSpeedElement.innerHTML = `${response.data.current.wind_mph}km/h`;
   humidityElement.innerHTML = `${response.data.current.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.current.wind_speed}km/h`;
-  let iconElement = document.querySelector("#icon");
-  iconElement.innerHTML = `<img src="${response.data.current.weather_icons[0]}" class="weather-app-icon" />`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let timeElement = document.querySelector("#time");
+
+  let date = new Date(response.data.location.localtime);
+  timeElement.innerHTML = formatDate(date);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.current.condition.text;
+  let iconElement = document.querySelector(".weather-app-icon");
+  let iconUrl = response.data.current.condition.icon;
+
+  iconElement.src = `https:${iconUrl}`;
+  getForecast(response.data.location.name);
 }
 
 function formatDate(date) {
@@ -43,9 +44,9 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "c72eac60bd8f11a1d8d75b27289d900d";
+  let apiKey = "063d8a4225924dfc97512815241907";
+  let apiUrl = `https://api.weatherapi.com/v1/current.json?q=${city}&lang=en&key=${apiKey}`;
 
-  let apiUrl = `https://api.weatherstack.com/current?access_key=${apiKey}&query=${city}& units=m`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -56,5 +57,54 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "063d8a4225924dfc97512815241907";
+  let apiUrl = `https://api.weatherapi.com/v1/forecast.json?q=${city}&lang=en&key=${apiKey}&days=7&aqi=no&alerts=no`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.forecast.forecastday.forEach(function (day, index) {
+    if (index < 5) {
+      let dayOfWeek = formatDay(day.date_epoch);
+
+      forecastHtml =
+        forecastHtml +
+        `
+           <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${dayOfWeek}</div>
+            <div class="weather-forecast-icon">
+              <img src="https:${day.day.condition.icon}" alt="${
+          day.day.condition.text
+        }" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.day.maxtemp_c)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.day.mintemp_c
+            )}º</div>
+          </div>
+        </div>
+      `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
+
