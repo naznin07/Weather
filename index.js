2571981e8a7ee2a1776c8ebe62e645d84a212b6d
naznin07 <naffect1@gmail.com>
2024-08-02 12:48:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-function updateTime() {
-  // Los Angeles
-  let losAngelesElement = document.querySelector("#los-angeles");
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.37/moment-timezone-with-data-1970-2030.min.js"></script>
+    <title>World Clock</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>World Clock</h1>
 
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+      <select id="city">
+        <option value="">Select a city..</option>
+        <option value="current">My current location</option>
+        <option value="Australia/Sydney">Sydney</option>
+        <option value="Europe/London">London</option>
+        <option value="Pacific/Auckland">Auckland</option>
+      </select>
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
-  // Paris
-  let parisElement = document.querySelector("#paris");
+      <div id="cities">
+        <div class="city" id="los-angeles">
+          <div>
+            <h2>Los Angeles</h2>
+            <div class="date"></div>
+          </div>
+          <div class="time"></div>
+        </div>
+        <div class="city" id="paris">
+          <div>
+            <h2>Paris</h2>
+            <div class="date"></div>
+          </div>
+          <div class="time"></div>
+        </div>
+      </div>
 
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-
-  parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small>]");
-}
-updateTime();
-setInterval(updateTime, 1000);
+      <footer>
+        This project was coded by
+        <a href="https://github.com/naznin07" target="_blank"
+          >Naznin Sultana,</a
+        >
+        and is
+        <a
+          href="https://github.com/naznin07/App/tree/World-Clock"
+          target="_blank"
+        >
+          open-sourced on GitHub</a
+        >
+        and
+        <a
+          href="https://app.netlify.com/teams/naffect1/overview"
+          target="_blank"
+          >hosted on Netlify</a
+        >
+      </footer>
+    </div>
+    <script src="index.js"></script>
+  </body>
+</html>
